f8c4c554388e19c1274568a8db41f84db0e5eb0a
nickballeste <nickballeste@gmail.com>
2018-08-13 17:46:18 -0300

    fixes last pieces of layout

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('.call-to-action').on({
+    mouseenter: function () {
+        $('h3').css({'color' : 'rgb(60, 163, 231)', 'font-size': '110%'});
+        $('a').css({'color' : 'rgb(60, 163, 231)', 'font-size': '110%'});
+    },
+});
