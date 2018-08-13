2b260101606281d34216ab6033cef718605f0209
nickballeste <nickballeste@gmail.com>
2018-08-13 19:08:07 -0300

    implements first media queries for 1024 and 1440

--- js/index.js
+++ js/index.js
@@ -3,4 +3,8 @@ $('.call-to-action').on({
         $('h3').css({'color' : 'rgb(60, 163, 231)', 'font-size': '110%'});
         $('a').css({'color' : 'rgb(60, 163, 231)', 'font-size': '110%'});
     },
+    mouseleave: function () {
+        $('h3').css({'color' : 'rgb(63, 179, 255)', 'font-size': '1em'});
+        $('a').css({'color' : 'rgb(63, 179, 255)', 'font-size': '1em'});
+    }
 });
